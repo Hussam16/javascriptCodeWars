1c3ddabd9f2d1cd5e5b8897ad22bbfe97e172ee5
hossam <hossamabdelfattah15@gmail.com>
2022-11-14 02:46:13 +0200

    create phone number challange

---
+++ createPhoneNumber.js
@@ -0,0 +1,19 @@
+
+let numbers=[10,5,26,35];
+
+
+let firstName="hussam";
+
+let arrayNumbers=[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+let word="";
+
+for (let index = 0; index < arrayNumbers.length; index++) {
+
+    word=word+arrayNumbers[index];
+    
+    
+}
+let phoneNumber="("+word.substring(0,3)+")"+" "+word.substring(3,6)+"-"+word.substring(6,10)
+console.log(phoneNumber)
+
